f38e601b4ecaae5249138b5120805116d0bd146e
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-06-04 17:05:40 +0530

    Add files via upload

---
+++ Day2/Democlassno.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+class Democlassno extends Component
+{
+ render()
+ {
+    return ( <img src={require('./welcome.jpg')}></img>)
+ }
+
+}
+export default Democlassno;
+
+
+
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRn0A5CPHf-6q_Zxe8iue2fCv24a6MgYi8xg&s
