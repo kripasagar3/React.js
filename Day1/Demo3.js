75d755ee1eb05c356b3238fb635836b06e7d1f75
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-06-04 17:04:26 +0530

    Add files via upload

---
+++ Day1/Demo3.js
@@ -0,0 +1,9 @@
+import React,{ Component } from "react";
+class Demo3 extends Component
+{
+    render()
+    {
+        return <p>Hi this is from <sub>class</sub></p>
+    }
+}
+export default Demo3;
