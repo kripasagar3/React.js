02558f96989814f97d6729fa1273acd9ac4aea01
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-06-05 18:07:21 +0530

    Add files via upload

---
+++ Day3/Childcomp.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+ function Childdisp(props)
+{
+    return(
+        <>
+        <button onClick={props.send}>click</button>
+        </>
+    )
+};
+export default Childdisp;
